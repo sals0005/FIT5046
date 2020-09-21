915f6d34db5452d49c53c1d3ce798c2c8d0e1737
Sarah <sals0005@student.monash.edu>
2020-09-21 14:12:21 +0300

    add project

---
+++ CaloriTracker3/src/java/entity/CalorisDurarion.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.math.BigDecimal;
+import javax.xml.bind.annotation.XmlRootElement;
+@XmlRootElement
+/**
+ *
+ * @author sarah
+ */
+public class CalorisDurarion {
+
+BigDecimal totalcaloriesconsumed;
+BigDecimal totalcaloriesburned;
+int totalstepstaken;
+
+
+public BigDecimal getTotalcaloriesconsumed() {
+return totalcaloriesconsumed;
+}
+public void setTotalcaloriesconsumed(BigDecimal totalcaloriesconsumed) {
+this.totalcaloriesconsumed = totalcaloriesconsumed;
+}
+public BigDecimal getTotalcaloriesburned() {
+return totalcaloriesburned;
+}
+public void setTotalcaloriesburned(BigDecimal totalcaloriesburned) {
+this.totalcaloriesburned = totalcaloriesburned;
+}
+
+public int getTotalstepstaken() {
+return totalstepstaken;
+}
+public void setTotalstepstaken(int totalstepstaken) {
+this.totalstepstaken = totalstepstaken;
+}
+public CalorisDurarion() {
+}
+
+public CalorisDurarion(BigDecimal totalcaloriesconsumed, BigDecimal totalcaloriesburned, int totalstepstaken) {
+this.totalcaloriesconsumed = totalcaloriesconsumed;
+this.totalcaloriesburned = totalcaloriesburned;
+this.totalstepstaken = totalstepstaken;
+}
+
+}
